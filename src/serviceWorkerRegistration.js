45eaff492b6d252d765aec41d7273f31bf446237
denziecodes <denziecodes@gmail.com>
2026-03-13 07:47:29 +0100

    Create serviceWorkerRegistration.js

---
+++ src/serviceWorkerRegistration.js
@@ -0,0 +1,11 @@
+// Default CRA PWA service worker registration
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+export function register(config) {
+  if ('serviceWorker' in navigator)
